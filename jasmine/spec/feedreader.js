cbf573fedb1a2816c8f123e8c3b17fa11bfedffc
aicute221 <aicute221@sina.cn>
2019-04-20 16:00:05 +0800

    错误更改

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -20,27 +20,29 @@ $(function () {
          * page?
          */
         it('are defined', function () {
-            expect(allFeeds).toBeDefined();
-            expect(allFeeds.length).not.toBe(0);
+            // expect(allFeeds).toBeDefined();
+            // expect(allFeeds.length).not.toBe(0);
+            notEmpty(allFeeds);
         });
 
         // 遍历 allFeeds 对象里面的所有的源来保证有链接字段而且链接不是空的
         it('is url defined', function () {
             allFeeds.forEach(function (Feed) {
-                let url = Feed.url;
-                expect(url).toBeDefined();
-                expect(url.length).not.toBe(0);
+                notEmpty(Feed.url);
             })
         });
 
         // 遍历 allFeeds 对象里面的所有的源来保证有名字字段而且不是空的
         it('is name defined', function () {
             allFeeds.forEach(function (Feed) {
-                let name = Feed.name;
-                expect(name).toBeDefined();
-                expect(name.length).not.toBe(0);
+                notEmpty(Feed.name);
             })
-        })
+        });
+
+        function notEmpty(data){
+            expect(data).toBeDefined();
+            expect(data.length).not.toBe(0);
+        }
     });
 
     /* test suite named "The menu" */
@@ -48,23 +50,21 @@ $(function () {
         let body;
 
         beforeEach(function () {
-            body = document.getElementsByTagName("body");
+            body = $('body');
         });
 
         //菜单元素默认是隐藏的
         it('is hide', function () {
-            expect(body[0].className).toBe('menu-hidden');
+            expect(body.hasClass('menu-hidden')).toBeTruthy();
         });
 
         //当菜单图标被点击的时候菜单会切换可见状态。
         it('is toggle', function () {
             $('.menu-icon-link').trigger('click');
-            // body = document.getElementsByTagName("body");
-            expect(body[0].className).not.toBe('menu-hidden');
+            expect(body.hasClass('menu-hidden')).toBeFalsy();
 
             $('.menu-icon-link').trigger('click');
-            // body = document.getElementsByTagName("body");
-            expect(body[0].className).toBe('menu-hidden');
+            expect(body.hasClass('menu-hidden')).toBeTruthy();
         })
     });
 
@@ -74,14 +74,11 @@ $(function () {
         //loadFeed 函数被调用而且工作正常，即在 .feed 容器元素里面至少有一个 .entry 的元素。
 
         beforeEach(function (done) {
-            loadFeed(0, function () {
-                done();
-            })
+            loadFeed(0,done)
         });
 
-        it('loadFeed works well', function (done) {
+        it('loadFeed works well', function () {
             expect($('.feed').children().length).not.toBe(0);
-            done();
         })
     });
 
@@ -90,17 +87,19 @@ $(function () {
     describe('New Feed Selection', function () {
         //用 loadFeed 函数加载一个新源的时候内容会改变
 
-        let child = $('.feed').children();
+        var content;
 
         beforeEach(function (done) {
-            loadFeed(1, function () {
-                done();
+            loadFeed(0,function(){
+                content = $('.feed').html();
+                // loadFeed 1 时接口返回内容有问题，导致不会走回调函数
+                // 所以这里使用了 loadFeed 2
+                loadFeed(2,done);
             })
         });
 
-        it('loadFeed changed', function (done) {
-            expect($('.feed').children()).not.toBe(child);
-            done();
+        it('loadFeed changed', function () {
+            expect($('.feed').html()).not.toBe(content);
         });
     })
 }());
